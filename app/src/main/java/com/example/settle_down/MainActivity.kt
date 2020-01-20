70ee6f7ba2ac64b90340403ccc64017e64ab602d
lanxi <568289315wlx@gmail.com>
2020-01-19 21:00:38 -0500

    initial build

---
+++ app/src/main/java/com/example/settle_down/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.settle_down
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
