22f3ed52072fa5cfb8aefda3d5d1bc02a16158a9
lanxi <568289315wlx@gmail.com>
2020-01-26 21:40:47 -0500

    Added Home

---
+++ app/src/main/java/com/example/settle_down/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.settle_down
+
+object Constants {
+    const val TAG = "SD"
+}
--- app/src/main/java/com/example/settle_down/MainActivity.kt
+++ app/src/main/java/com/example/settle_down/MainActivity.kt
@@ -2,9 +2,14 @@ package com.example.settle_down
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val auth = FirebaseAuth.getInstance()
+    lateinit var authStateListener: FirebaseAuth.AuthStateListener
+    private val RC_SIGN_IN = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +21,45 @@ class MainActivity : AppCompatActivity() {
             ft.add(R.id.fragment_container, startingFragment)
             ft.commit()
         }
+        initializeListeners()
     }
+
+    override fun onStart() {
+        super.onStart()
+        auth.addAuthStateListener (authStateListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        auth.removeAuthStateListener(authStateListener)
+    }
+
+    private fun initializeListeners() {
+        // TODO: Create an AuthStateListener that passes the UID
+        // to the MovieQuoteFragment if the user is logged in
+        // and goes back to the Splash fragment otherwise.
+        // See https://firebase.google.com/docs/auth/users#the_user_lifecycle
+        authStateListener = FirebaseAuth.AuthStateListener {auth: FirebaseAuth ->
+            val user = auth.currentUser
+            Log.d(Constants.TAG, "In anth listener, user = $user")
+            if(user != null){
+                Log.d(Constants.TAG, "UID: ${user.uid}")
+                Log.d(Constants.TAG, "Name: ${user.displayName}")
+                Log.d(Constants.TAG, "Email: ${user.email}")
+                Log.d(Constants.TAG, "Phone: ${user.phoneNumber}")
+                Log.d(Constants.TAG, "Photo URL: ${user.photoUrl}")
+                switchToHomeFragment(user.uid)
+            }else{
+//                switchToFragment()
+            }
+        }
+
+    }
+
+    private fun switchToHomeFragment(uid: String) {
+        val ft = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.fragment_container, HomeFragment.newInstance(uid))
+        ft.commit()
+    }
+
 }
