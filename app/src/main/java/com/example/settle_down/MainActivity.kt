2ac5bc9c37606cb76aef6b00e8a3081c3612a1ca
lanxi <568289315wlx@gmail.com>
2020-01-26 20:30:53 -0500

    to fragment

--- app/src/main/java/com/example/settle_down/MainActivity.kt
+++ app/src/main/java/com/example/settle_down/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.settle_down
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        if(savedInstanceState == null) {
+            val startingFragment = StartingFragment()
+            val ft = supportFragmentManager.beginTransaction()
+            ft.add(R.id.fragment_container, startingFragment)
+            ft.commit()
+        }
     }
 }
